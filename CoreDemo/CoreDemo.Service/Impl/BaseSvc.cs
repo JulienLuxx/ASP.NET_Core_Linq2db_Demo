e3e147d5516d01f0e13a33f015d69233be9f9bd6
JulienLuxx <JulienLux@outlook.com>
2019-09-05 18:45:59 +0800

    AddSubitem&AddT4GeneratedEntityFile

---
+++ CoreDemo/CoreDemo.Service/Impl/BaseSvc.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreDemo.Service
+{
+    public abstract class BaseSvc
+    {
+        protected IMapper _mapper;
+
+        protected BaseSvc()
+        { }
+    }
+}
