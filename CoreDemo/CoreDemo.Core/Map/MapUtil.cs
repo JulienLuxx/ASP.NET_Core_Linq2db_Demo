67589ab2e2b8daede3457b7553ec76d9bc287156
JulienLuxx <JulienLux@outlook.com>
2019-09-05 11:02:40 +0800

    AddSomeBaseInterface&SomeUtil

---
+++ CoreDemo/CoreDemo.Core/Map/MapUtil.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace CoreDemo.Core
+{
+    public class MapUtil : IMapUtil
+    {
+        public IDictionary<string, string> DynamicToDictionary(dynamic obj)
+        {
+            IDictionary<string, string> dict = new Dictionary<string, string>();
+
+            var type = obj.GetType();
+            var propertys = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            foreach (var property in propertys)
+            {
+                var method = property.GetGetMethod();
+                if (null != method && method.IsPublic)
+                {
+                    if (null != property.GetValue(obj))
+                    {
+                        dict.Add(property.Name, property.GetValue(obj).ToString());
+                    }
+                }
+            }
+            return dict;
+        }
+
+        public List<string> DynamicToStringList(dynamic obj)
+        {
+            var list = new List<string>();
+            var type = obj.GetType();
+            var propertys = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            foreach (var property in propertys)
+            {
+                var method = property.GetGetMethod();
+                if (null != method && method.IsPublic)
+                {
+                    if (null != property.GetValue(obj))
+                    {
+                        var str = property.Name + "=" + property.GetValue(obj).ToString();
+                        list.Add(str);
+                    }
+                }
+            }
+            return list;
+        }
+    }
+}
